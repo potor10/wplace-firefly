d977ce69cc3e28c638f0d5c7c287d45faa8f2ec4
Potor <liupotor4@gmail.com>
2025-08-16 08:57:41 -0700

    Add suspense boundary

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,9 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body>{children}</body>
+            <React.Suspense>
+                <body>{children}</body>
+            </React.Suspense>
         </html>
     )
 }
